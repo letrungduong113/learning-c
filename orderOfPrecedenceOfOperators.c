ca8f4d475363becd83f8e2c2cec42e4a57c5f2a2
DuongLT <letrungduongxx3@gmail.com>
2021-05-16 21:22:23 +0700

    Thứ tự ưu tiên của các toán tử update

--- orderOfPrecedenceOfOperators.c
+++ orderOfPrecedenceOfOperators.c
@@ -1,1 +1,8 @@
 // Thứ tự ưu tiên của các toán tử
+
+/*
+* Dấu () có mức ưu tiên cao nhất. Nếu biểu thức có nhiều cặp dấu () thì cặp dấu () trong cùng sẽ có múc ưu tiên cao nhất
+* Tiếp theo là *, /, % thì có cùng mức ưu tiên. Nếu trong biểu thức có nhiêu *, /, %  thì sẽ lần lượt thực hiện từ trái qua phải
+* Kế đến là +, -, nếu trong biểu thức có nhiêu +, -  thì sẽ lần lượt thực hiện từ trái qua phải
+* Cuối cùng là = có mức ưu tiên nhỏ nhất, thường mỗi biểu thức chỉ có 1 phép gán
+*/
