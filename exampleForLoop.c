b9da87f728411f988399c5c1857fbb8db608aa25
DuongLT <letrungduongxx3@gmail.com>
2021-05-25 23:49:31 +0700

    Ex 2 example for loop

--- exampleForLoop.c
+++ exampleForLoop.c
@@ -11,3 +11,17 @@ int main() {
 
     printf("Tổng %d\n", sum);
 }
+
+// Nhập 1 số nguyên n, xuất tổng các số lẻ từ 1 đến n
+
+int main() {
+    int n, i, sum = 0;
+    puts("Nhập vào số nguyên: ");
+    scanf("%d", &n);
+    for(i = 0; i <= n; ++i) {
+        if(i % 2 != 0) {
+            sum += i;
+        }
+    }
+    printf("%d\n", sum);
+}
