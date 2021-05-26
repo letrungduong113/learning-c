fdba811a392af55e553ef0e1cc5fa70be1fa8d22
DuongLT <letrungduongxx3@gmail.com>
2021-05-26 23:27:59 +0700

    Ex 4 example for loop

--- exampleForLoop.c
+++ exampleForLoop.c
@@ -12,7 +12,7 @@ int main() {
     printf("Tổng %d\n", sum);
 }
 
-// Nhập 1 số nguyên n, xuất tổng các số lẻ từ 1 đến n
+//Bài 2: Nhập 1 số nguyên n, xuất tổng các số lẻ từ 1 đến n
 
 int main() {
     int n, i, sum = 0;
@@ -26,7 +26,7 @@ int main() {
     printf("%d\n", sum);
 }
 
-// Nhập vào số nguyên n, kiểm tra xem phải là số nguyên tố hay không
+//Bài 3: Nhập vào số nguyên n, kiểm tra xem phải là số nguyên tố hay không
 
 
 int main() {
@@ -42,4 +42,24 @@ int main() {
         if(k == 2) 
             printf("Là số nguyên tố\n");
          else printf("Không phải số nguyên tố\n");
+}
+
+// Bài 4: Nhập số tự nhiên n sau đó in ra các số chẵn nhỏ hơn n và các số lẻ nhỏ
+// hơn n.
+int main () {
+    int a;
+    puts("Nhập vào số tự nhiên: ");
+    scanf("%d", &a);
+    if(a < 0) {
+        printf("Đây không phải số tự nhiên");
+    } else {
+        for(int i = 0; i < a; ++i) {
+            if(i % 2 == 0) continue;
+            printf("Số lẻ là %d\n", i);
+        }
+        for(int i = 0; i < a; ++i) {
+            if(i % 2 != 0) continue;
+            printf("Số chẵn là %d\n", i);
+        }
+    }
 }
