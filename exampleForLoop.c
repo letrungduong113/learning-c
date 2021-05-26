9f1c9f6ba6a02c05ab1a1249ae1e923a68a20c7b
DuongLT <letrungduongxx3@gmail.com>
2021-05-26 23:21:41 +0700

    Ex 3 example for loop

--- exampleForLoop.c
+++ exampleForLoop.c
@@ -25,3 +25,21 @@ int main() {
     }
     printf("%d\n", sum);
 }
+
+// Nhập vào số nguyên n, kiểm tra xem phải là số nguyên tố hay không
+
+
+int main() {
+        int a, i, k = 0;
+        puts("Nhập vào số nguyên: ");
+        scanf("%d", &a);
+
+        for(i = 1; i <= a; ++i) {
+            if(a % i == 0) {
+                ++k;
+            }
+        }
+        if(k == 2) 
+            printf("Là số nguyên tố\n");
+         else printf("Không phải số nguyên tố\n");
+}
